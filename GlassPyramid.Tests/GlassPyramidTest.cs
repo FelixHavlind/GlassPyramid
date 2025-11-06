87b475466ead3f6dbf3c7ce1b5325106a3e71c8f
Felix Havlind <havlindfelix@gmail.com>
2025-11-06 11:35:55 +0100

    Implementation of unit test.

---
+++ GlassPyramid.Tests/GlassPyramidTest.cs
@@ -0,0 +1,20 @@
+﻿namespace GlassPyramid.Tests;
+
+public class GlassPyramidTest
+{
+    private readonly GlassPyramid _glassPyramid = new(5);
+
+    [Theory]
+    [InlineData(0, 0, 10.0)]
+    [InlineData(1, 0, 30.0)]
+    [InlineData(3, 1, 83.333)]
+    [InlineData(4, 0, 310.0)]
+    public void Simulate_ShouldReturnExpectedDecimal_ForMultipleInputTests(int targetDepth, int targetIndex, decimal expectedResult)
+    {
+        // ACT
+        var actualResult = _glassPyramid.Simulate(targetDepth, targetIndex);
+        
+        // ASSERT
+        Assert.Equal(expectedResult, actualResult, precision: 3);
+    }
+}
